8d97e47b0e4329be572ae101aafad28a2a83cfe8
Blair Reid <blair.reid@shaw.ca>
2017-01-12 08:18:25 -0800

    Initial View Controller setup

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Blair Reid on 2017-01-12.
+//  Copyright © 2017 Blair Reid. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+ 
+    @IBAction func playTapped(_ sender: Any) {
+    }
+   
+    @IBAction func addTapped(_ sender: Any) {
+    }
+  
+    
+}
